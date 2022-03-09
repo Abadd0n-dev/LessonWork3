c119a098849082f2d015d51637d406f04a716775
rkutaraev@list.ru <rkutaraev@list.ru>
2022-03-09 15:37:33 +0300

    Написал программу, выводящую двухмерный массив по диагонали.

--- LessonWork3/Program.cs
+++ LessonWork3/Program.cs
@@ -10,6 +10,23 @@ namespace LessonWork3
     {
         static void Main(string[] args)
         {
+            int[,] matrix = new int[5, 5];
+
+            matrix[0, 0] = 1;
+            matrix[1, 1] = 1;
+            matrix[2, 2] = 1;
+            matrix[3, 3] = 1;
+            matrix[4, 4] = 1;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
